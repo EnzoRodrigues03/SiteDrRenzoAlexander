ee3cbf02c871574f47a574822db29691b0c34f52
EnzoRodrigues03 <wepoethypo@gmail.com>
2025-07-25 12:02:11 -0300

    Add files via upload

--- sidebar.js
+++ sidebar.js
@@ -13,3 +13,103 @@ function clicar2() {
         mover.classList.remove("ativa")
     }
 }
+
+function clicaresp() {
+
+// Cabeçalho
+
+window.document.body.querySelector("a#sobremim").innerHTML = "Acerca de mí"
+window.document.body.querySelector("a#servicos").innerHTML = "Servicios";
+window.document.body.querySelector("a#missao").innerHTML = "Misión | Visión | Valores";
+window.document.body.querySelector("a#diferenciais").innerHTML = "Diferencias";
+window.document.body.querySelector("a#contato").innerHTML = "Contacto";
+
+//Seção 1
+
+window.document.body.querySelector("h1#s1h1").innerHTML = "DERECHO CON RESPONSABILIDAD Y COMPROMISO";
+window.document.body.querySelector("p#s1p1").innerHTML = "Práctica en Derecho Administrativo, Derecho Constitucional, Derecho de la Seguridad Social, Derecho Laboral, Migración y Naturalización, Derecho de Familia";
+
+//Seção 2
+
+window.document.body.querySelector("h1#s2h1").innerHTML = "La Constitución es mi tierra. La dignidad es mi idioma.";
+window.document.body.querySelector("p#s2p1").innerHTML = "Renzo Alexander Moreno Oberto Naturalizado venezolano en el mundo.";
+window.document.body.querySelector("p#s2p2").innerHTML = "Abogo por la intersección del orden constitucional y el principio de dignidad. Soy licenciado y posgraduado en Derecho Administrativo y Derecho Público por la Universidad de Mogi das Cruzes y aprobé el 42.º examen de la OAB.";
+window.document.body.querySelector("p#s2p3").innerHTML = "Brasileño de alma crítica, creo que el derecho no es sólo una norma: es lenguaje, es presencia, es un instrumento de libertad institucional.";
+
+//Seção 3
+
+window.document.body.querySelector("h1#s3h1txt1").innerHTML = "Derecho Constitucional";
+window.document.body.querySelector("p#s3ptxt1").innerHTML = "• Control de constitucionalidad";
+window.document.body.querySelector("p#s3ptxt2").innerHTML = "• Recursos constitucionales";
+window.document.body.querySelector("p#s3ptxt3").innerHTML = "• Protección de los derechos fundamentales";
+window.document.body.querySelector("p#s3ptxt4").innerHTML = "• Derechos fundamentales de la diversidad";
+
+window.document.body.querySelector("h1#s3h1txt2").innerHTML = "Derecho Administrativo";
+window.document.body.querySelector("p#s3ptxt5").innerHTML = "• Licitaciones y Contratos Administrativos";
+window.document.body.querySelector("p#s3ptxt6").innerHTML = "• Responsabilidad Civil del Estado";
+window.document.body.querySelector("p#s3ptxt7").innerHTML = "• Impropiedad administrativa";
+window.document.body.querySelector("p#s3ptxt8").innerHTML = "• Intervención estatal";
+window.document.body.querySelector("p#s3ptxt9").innerHTML = "• Consultoría de Políticas Públicas y Gobernanza";
+
+window.document.body.querySelector("h1#s3h1txt3").innerHTML = "Ley de Seguridad Social";
+window.document.body.querySelector("p#s3ptxt10").innerHTML = "• Beneficios del Régimen General de la Seguridad Social (RGPS)";
+window.document.body.querySelector("p#s3ptxt11").innerHTML = "• Planes de Pensiones Propios de los Funcionarios Públicos (RPPS)";
+window.document.body.querySelector("p#s3ptxt12").innerHTML = "• BPC/LOAS";
+
+window.document.body.querySelector("h1#s3h1txt4").innerHTML = "Migración y naturalización";
+window.document.body.querySelector("p#s3ptxt13").innerHTML = "• Naturalización ordinaria, extraordinaria y especial";
+window.document.body.querySelector("p#s3ptxt14").innerHTML = "• Visas, Residencia y Regularización Migratoria";
+window.document.body.querySelector("p#s3ptxt15").innerHTML = "• Derechos constitucionales de los extranjeros";
+window.document.body.querySelector("p#s3ptxt16").innerHTML = "• Expulsión, Deportación y Repatriación";
+window.document.body.querySelector("p#s3ptxt17").innerHTML = "• Revalidación de Diplomas y Documentos Extranjeros";
+
+window.document.body.querySelector("h1#s3h1txt5").innerHTML = "Derecho Laboral";
+window.document.body.querySelector("p#s3ptxt18").innerHTML = "• Acciones Laborales Individuales";
+window.document.body.querySelector("p#s3ptxt19").innerHTML = "• Asesoría Laboral para Empresas";
+window.document.body.querySelector("p#s3ptxt20").innerHTML = "• Accidentes de trabajo y enfermedades profesionales";
+
+window.document.body.querySelector("h1#s3h1txt6").innerHTML = "Derecho de Familia";
+window.document.body.querySelector("p#s3ptxt21").innerHTML = "• Filiación, Paternidad y Parentalidades";
+window.document.body.querySelector("p#s3ptxt22").innerHTML = "• Custodia, convivencia y poder familiar";
+window.document.body.querySelector("p#s3ptxt23").innerHTML = "• Sucesión e Inventario";
+window.document.body.querySelector("p#s3ptxt24").innerHTML = "• Adopción nacional e internacional";
+
+window.document.body.querySelector("h1#s3h1txt").innerHTML = "Compliance de asesoramiento";
+window.document.body.querySelector("p#s3ptxt25").innerHTML = "• Elaboración de Códigos de Conducta y Políticas Internas";
+window.document.body.querySelector("p#s3ptxt26").innerHTML = "• Canal de denuncia y procedimientos de investigación interna";
+window.document.body.querySelector("p#s3ptxt27").innerHTML = "• Cumplimiento de la LGPD";
+window.document.body.querySelector("p#s3ptxt28").innerHTML = "• Compliance anticorrupción e integridad pública";
+window.document.body.querySelector("p#s3ptxt29").innerHTML = "• Formación y cualificación interna";
+
+//Seção 4
+
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+
+//Seção 5
+
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+window.document.body.querySelector().innerHTML = "";
+}
